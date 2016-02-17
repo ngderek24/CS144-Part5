141dcce71b3b4a2c14015883fbef00eb5369053a
jasony0206 <jasony0206@gmail.com>
2016-02-16 22:24:45 -0800

    Query form sends to SearchServlet

--- src/edu/ucla/cs/cs144/SearchServlet.java
+++ src/edu/ucla/cs/cs144/SearchServlet.java
@@ -1,6 +1,7 @@
 package edu.ucla.cs.cs144;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +14,13 @@ public class SearchServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>request received by search servlet!</body></html>");
+        String q = request.getParameter("q");
+        String numResultsToSkip = request.getParameter("numResultsToSkip");
+        String numResultsToReturn = request.getParameter("numResultsToReturn");
+        out.println("q: " + q);
+        out.println("numResultsToSkip: " + numResultsToSkip);
+        out.println("numResultsToReturn: " + numResultsToReturn);
     }
 }
