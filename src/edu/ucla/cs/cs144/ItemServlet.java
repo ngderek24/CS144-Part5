c011c4f4e055ae55571121978b07387ff77bac16
tincan24 <tincan24@gmail.com>
2016-02-17 00:19:30 -0800

    Added XMLBean.java and modified ItemServlet.java

--- src/edu/ucla/cs/cs144/ItemServlet.java
+++ src/edu/ucla/cs/cs144/ItemServlet.java
@@ -1,11 +1,24 @@
 package edu.ucla.cs.cs144;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import java.util.*;
+import org.w3c.dom.Node;
+import org.w3c.dom.Text;
 
 public class ItemServlet extends HttpServlet implements Servlet {
        
@@ -13,6 +26,99 @@ public class ItemServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        PrintWriter out = response.getWriter();
+        String itemID = request.getParameter("itemid");
+        String xmlData = AuctionSearchClient.getXMLDataForItemId(itemID);
+        String name = processData(xmlData);
+
+        //request.setAttribute("name", xmlData);
+        //request.getRequestDispatcher("item.jsp").forward(request, response);
+        out.println(name);
+    }
+
+    private static String processData(String xmlData) {
+        Document doc = null;
+        try {
+            doc = loadXMLFromString(xmlData);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(3);
+        }
+        catch (SAXException e) {
+            System.out.println("Parsing error on file " + xmlData);
+            System.out.println("  (not supposed to happen with supplied XML files)");
+            e.printStackTrace();
+            System.exit(3);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(3);
+        }
+
+        Element item = doc.getDocumentElement();
+        String name = getElementTextByTagNameNR(item, "Name");
+        return name;
+    }
+
+    private static Document loadXMLFromString(String xml) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml));
+        return builder.parse(is);
+    }
+
+    /* Non-recursive (NR) version of Node.getElementsByTagName(...)
+     */
+    static Element[] getElementsByTagNameNR(Element e, String tagName) {
+        Vector< Element > elements = new Vector< Element >();
+        Node child = e.getFirstChild();
+        while (child != null) {
+            if (child instanceof Element && child.getNodeName().equals(tagName))
+            {
+                elements.add( (Element)child );
+            }
+            child = child.getNextSibling();
+        }
+        Element[] result = new Element[elements.size()];
+        elements.copyInto(result);
+        return result;
+    }
+
+    /* Returns the first subelement of e matching the given tagName, or
+     * null if one does not exist. NR means Non-Recursive.
+     */
+    static Element getElementByTagNameNR(Element e, String tagName) {
+        Node child = e.getFirstChild();
+        while (child != null) {
+            if (child instanceof Element && child.getNodeName().equals(tagName))
+                return (Element) child;
+            child = child.getNextSibling();
+        }
+        return null;
+    }
+
+    /* Returns the text associated with the given element (which must have
+     * type #PCDATA) as child, or "" if it contains no text.
+     */
+    static String getElementText(Element e) {
+        if (e.getChildNodes().getLength() == 1) {
+            Text elementText = (Text) e.getFirstChild();
+            return elementText.getNodeValue();
+        }
+        else
+            return "";
+    }
+
+    /* Returns the text (#PCDATA) associated with the first subelement X
+     * of e with the given tagName. If no such X exists or X contains no
+     * text, "" is returned. NR means Non-Recursive.
+     */
+    static String getElementTextByTagNameNR(Element e, String tagName) {
+        Element elem = getElementByTagNameNR(e, tagName);
+        if (elem != null)
+            return getElementText(elem);
+        else
+            return "";
     }
 }
---
+++ src/edu/ucla/cs/cs144/XMLBean.java
@@ -0,0 +1,5 @@
+package edu.ucla.cs.cs144;
+
+public class XMLBean implements java.io.Serializable {
+    private String itemID, name, currently, buyPrice, firstBid, numOfBids, location, country, started, ends;
+}
