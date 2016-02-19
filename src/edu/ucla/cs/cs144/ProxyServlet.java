616c30c6db3977175894173be5c015f008c796f4
tincan24 <tincan24@gmail.com>
2016-02-18 20:37:37 -0800

    Successfully received Google suggest XML data

--- src/edu/ucla/cs/cs144/ProxyServlet.java
+++ src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -1,7 +1,12 @@
 package edu.ucla.cs.cs144;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +19,26 @@ public class ProxyServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        PrintWriter out = response.getWriter();
+        String q = request.getParameter("q");
+
+        if (q.equals(""))
+            return;
+
+        String url = "http://www.google.com/complete/search?output=toolbar&";
+        String charset = "UTF-8";
+        String query = String.format("q=%s", URLEncoder.encode(q, charset));
+        URLConnection conn = new URL(url + query).openConnection();
+        conn.setRequestProperty("Accept-Charset", charset);
+        InputStream googleResponse = conn.getInputStream();
+
+        java.util.Scanner s = new java.util.Scanner(googleResponse, charset).useDelimiter("\\A");
+        String xmlData = s.hasNext() ? s.next() : "";
+
+        int status = ((HttpURLConnection) conn).getResponseCode();
+        response.setStatus(status);
+        out.write(xmlData);
+        out.flush();
+        out.close();
     }
 }
