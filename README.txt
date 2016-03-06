755fa1f04157f2cdd429f927548f665a9cbacfe1
Derek Nguyen <tincan24@gmail.com>
2016-03-06 12:25:34 -0800

    Answered Q2 in README.txt and removed isSecure() function call

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 Q1: (4) -> (5)
 	(5) -> (6)
 
-Q2: 
+Q2: We ensured that the item was purchased exactly at the Buy_Price by obtaining the Buy_Price value directly from the server when we retrieved the item information and storing it in the HTTP session for later accesses. Using HTTP session is also a way of defending against client state manipulation. The user has no way of changing the value of the Buy_Price in the session.
--- src/edu/ucla/cs/cs144/ConfirmationServlet.java
+++ src/edu/ucla/cs/cs144/ConfirmationServlet.java
@@ -15,26 +15,22 @@ public class ConfirmationServlet extends HttpServlet implements Servlet {
     public ConfirmationServlet() {}
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.isSecure()) {
-            HttpSession session = request.getSession(true);
-            String itemID = (String) session.getAttribute("itemID");
-            String name = (String) session.getAttribute("name");
-            String buyPrice = (String) session.getAttribute("buyPrice");
-            String creditCardNumber = (String) request.getParameter("creditCardNumber");
+        HttpSession session = request.getSession(true);
+        String itemID = (String) session.getAttribute("itemID");
+        String name = (String) session.getAttribute("name");
+        String buyPrice = (String) session.getAttribute("buyPrice");
+        String creditCardNumber = (String) request.getParameter("creditCardNumber");
 
-            request.setAttribute("itemID", itemID);
-            request.setAttribute("name", name);
-            request.setAttribute("buyPrice", buyPrice);
-            request.setAttribute("creditCardNumber", creditCardNumber);
+        request.setAttribute("itemID", itemID);
+        request.setAttribute("name", name);
+        request.setAttribute("buyPrice", buyPrice);
+        request.setAttribute("creditCardNumber", creditCardNumber);
 
-            Calendar cal = Calendar.getInstance();
-            SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy hh:mm:ss aaa");
-            String transactionTime = sdf.format(cal.getTime());
-            request.setAttribute("transactionTime", transactionTime);
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy hh:mm:ss aaa");
+        String transactionTime = sdf.format(cal.getTime());
+        request.setAttribute("transactionTime", transactionTime);
 
-            request.getRequestDispatcher("confirmation.jsp").forward(request, response);
-        } else {
-            request.getRequestDispatcher("error.html").forward(request, response);
-        }
+        request.getRequestDispatcher("confirmation.jsp").forward(request, response);
     }
 }
